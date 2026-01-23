c294df5c79eb1f2aada598805a03fcf9510f58d2
Gianni D'Amico <gianni.31@free.fr>
2026-01-23 19:26:00 +0100

    DEBUG: Disable middleware, add /ping

---
+++ src/app/ping/page.tsx
@@ -0,0 +1,8 @@
+export default function Ping() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">PONG</h1>
+            <p className="ml-4">Deployment is working.</p>
+        </div>
+    );
+}
